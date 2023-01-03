5ed7c4a2c2007a3d78bbb87d9409895e3a5ea368
AndreiCrimezniuc <kib69dev19@gmail.com>
2023-01-03 08:09:23 +0200

    added draft radioBox component

---
+++ src/model/station.ts
@@ -0,0 +1,15 @@
+export type Station = {
+    id: string,
+    name: string,
+    site?: string | null,
+    email?: string | null,
+    region?: string | null,
+    city?: string | null,
+    address?: string | null,
+    facebook?: string | null,
+    twitter?: string | null,
+    vk?: string | null,
+    wiki?: string | null,
+    phoneNumber?: string | null,
+    stream: string
+}
