ee68e76c28b4eb37ceccd3e5ce181bfcfa8dcadf
AndreiCrimezniuc <kib69dev19@gmail.com>
2023-01-05 22:24:40 +0200

    one more version

--- src/api/station/stationAPI.ts
+++ src/api/station/stationAPI.ts
@@ -12,21 +12,36 @@ export async function getStationById(id: string): Promise<Station> {
     return response.data;
 }
 
-// export async function getStations(): Promise<Station[]> {
-//     const response = await axios.get(apiUrl + `ru/stations`, {
-//         headers: {
-//             Accept: 'application/json'
-//         },
-//     });
-//
-//     return response.data;
-// }
+export function getStations(): Promise<Station[] | null> {
+    return axios.get(apiUrl + `ru/stations`, {
+        headers: {
+            Accept: 'application/json'
+        },
+    })
+        .then((res) => {
+            return res.data
+        }).catch(e => {
+        console.log(e)
+        return null
+    })
+}
 
-export const getStations = () =>
-    axios
-        .get(apiUrl + `ru/stations`, {
-            headers: {
-                Accept: 'application/json'
-            },
-        })
-        .then((res) => res?.data?.code);
+// export function getStations(): Promise<Station[]> {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             const stations: Station[] = [
+//                 {
+//                     id: "1",
+//                     name: "Station 1",
+//                     stream: "http://station1.com/stream",
+//                 },
+//                 {
+//                     id: "2",
+//                     name: "Station 2",
+//                     stream: "http://station2.com/stream",
+//                 }
+//             ];
+//             resolve(stations);
+//         }, 1000);
+//     });
+// }
--- src/component/radiobox/radiobox.tsx
+++ src/component/radiobox/radiobox.tsx
@@ -3,38 +3,24 @@ import {Station} from "../../model/station";
 import {getStations} from "../../api/station/stationAPI";
 
 export function RadioBox(): ReactElement {
-    const [stations, setStations] = useState<Station[]>()
-
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         try {
-    //             const data = await getStations();
-    //             setStations(data);
-    //         } catch (e) {
-    //             console.log("error IDK WHY", e)
-    //         }
-    //     }
-    //     fetchData();
-    // }, []);
+    const [stations, setStations] = useState<Station[] | null>(null);
 
     useEffect(() => {
-        getStations().then(res => setStations(res));
+        getStations().then(rep =>
+            setStations(rep)
+        )
     }, []);
-
-    let renderStations
-
-    if (stations) {
-         renderStations = stations.map(x => <li> <p>Name: {x.name} , <audio src={x.stream}> {x.id}</audio></p></li>)
-    } else {
-         renderStations = <div> "Nothing here" </div>
-    }
 
     return (
         <div className="radio-box">
             <p>Hello</p>
             <ul>
-                {renderStations}
+                {stations ? stations.map((x) => (
+                    <li key={x.id}>
+                        <p>Name: {x.name}, <audio src={x.stream}>{x.id}</audio></p>
+                    </li>
+                )) : <div>Nothing here</div>}
             </ul>
         </div>
-    )
+    );
 }
